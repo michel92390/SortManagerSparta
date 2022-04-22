d1c76450b2b402454443354bc54420add40adae9
michel j <michel.jean.uk@gmail.com>
2022-04-22 23:40:16 +0100

    improvment

--- src/main/java/com/sparta/sortmanager/content/GetTimes.java
+++ src/main/java/com/sparta/sortmanager/content/GetTimes.java
@@ -1,53 +1,37 @@
 package com.sparta.sortmanager.content;
 
 import com.sparta.sortmanager.binaryTree.BinaryTreeSort;
+import com.sparta.sortmanager.sorters.BubbleSort;
 import com.sparta.sortmanager.sorters.MergeSort;
 
 public class GetTimes {
-    public static double[] getAllTimes(int[] arrayToSort) {
-        double[] times = new double[2];
-        int[] arrayToSort2 = arrayToSort.clone();
-        int[] arrayToSort3 = arrayToSort.clone();
-        times[0] = getBubbleSortTime(arrayToSort);
-        times[1] = getMergeSortTime(arrayToSort2);
-        times[2] = getTreeSortTime(arrayToSort3);
-        return times;
-    }
 
     public static double getBubbleSortTime(int[] arrayToSort) {
-
+        BubbleSort bubbleSort = new BubbleSort();
         long startTime = System.nanoTime();
-        for (int i = 0; i < arrayToSort.length - 1; i++) {
-            for (int j = 0; j < arrayToSort.length - i - 1; j++) {
-                if (arrayToSort[j] > arrayToSort[j + 1]) {
-                    int temp = arrayToSort[j];
-                    arrayToSort[j] = arrayToSort[j + 1];
-                    arrayToSort[j + 1] = temp;
-                }
-            }
-        }
+
+        bubbleSort.sortArray(arrayToSort);
 
         long endTime = System.nanoTime();
         return (double) (endTime - startTime) / 1000000;
     }
 
     public static double getMergeSortTime(int[] arrayToSort) {
-        MergeSort ms = new MergeSort();
+        MergeSort mergeSort = new MergeSort();
         long startTime = System.nanoTime();
 
-        ms.mergeSort(arrayToSort, 0, arrayToSort.length - 1);
+        mergeSort.mergeSort(arrayToSort, 0, arrayToSort.length - 1);
 
         long endTime = System.nanoTime();
         return (double) (endTime - startTime) / 1000000;
     }
 
     public static double getTreeSortTime(int[] arrayToSort) {
-        BinaryTreeSort bti = new BinaryTreeSort();
-        bti.addElements(arrayToSort);
-
+        BinaryTreeSort binaryTreeSort = new BinaryTreeSort();
+        binaryTreeSort.addElements(arrayToSort);
         long startTime = System.nanoTime();
 
-        int[] sortedTree = bti.getSortedTreeAsc();
+        binaryTreeSort.getSortedTreeAsc();
 
         long endTime = System.nanoTime();
         return (double) (endTime - startTime) / 1000000;
--- src/main/java/com/sparta/sortmanager/content/RandomArray.java
+++ src/main/java/com/sparta/sortmanager/content/RandomArray.java
@@ -16,23 +16,23 @@ public class RandomArray {
             System.out.print("Length of the array between 2 and 20: ");
             int lengthArray = scan.nextInt();
             Random rand = new Random();
-                if(lengthArray == 0){
-                    System.out.println("The array could not be empty");
-                    logger.error("The array length("+lengthArray+") could not be empty");
-                } else if (lengthArray == 1) {
-                    System.out.println("The array should have more than 1 item.");
-                    logger.error("The array length should be longer than 1.");
-                } else if (lengthArray > 20) {
-                    System.out.println("The array should not have more than 20 items");
-                    logger.error("The array should not have more than 20 items");
-                } else {
-                    int[] arr = new int[lengthArray];
-                    for (int i = 0; i < arr.length; i++) {
-                        arr[i] = rand.nextInt(-100, 100);
-                    }
-                    lenArray = false;
-                    return arr;
+            if (lengthArray == 0) {
+                System.out.println("The array could not be empty");
+                logger.error("The array length(" + lengthArray + ") could not be empty");
+            } else if (lengthArray == 1) {
+                System.out.println("The array should have more than 1 item.");
+                logger.error("The array length should be longer than 1.");
+            } else if (lengthArray > 20) {
+                System.out.println("The array should not have more than 20 items");
+                logger.error("The array should not have more than 20 items");
+            } else {
+                int[] arr = new int[lengthArray];
+                for (int i = 0; i < arr.length; i++) {
+                    arr[i] = rand.nextInt(-100, 100);
                 }
+                lenArray = false;
+                return arr;
+            }
         }
         return null;
     }
